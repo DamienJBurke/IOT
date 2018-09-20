19befa42148f21d35b2b2612aa38df1e20e04093
DamienJBurke <damienburke94@gmail.com>
2018-09-20 23:27:13 +0100

    CleanUp

--- Wemos/Setup/main.py
+++ Wemos/Setup/main.py
@@ -2,33 +2,23 @@ import machine
 import time
 import ubinascii
 import webrepl
-
 from umqtt.simple import MQTTClient
-
+from dht import DHT11
+import ujson as json
 # These defaults are overwritten with the contents of /config.json by load_config()
 CONFIG = {
     "broker": "192.168.0.129",
-    "sensor_pin": 4, 
+    "sensor_pin": 0, 
     "client_id": b"esp8266_" + ubinascii.hexlify(machine.unique_id()),
-    "topic": "home",
+    "topic": "sensor-data",
 }
-
 client = None
-sensor_pin = None
-
-def setup_pins():
-    global sensor_pin
-    sensor_pin = machine.ADC(CONFIG['sensor_pin'])
-
 def current_temp_humidity():
-    from dht import DHT11
-    import machine
     d = DHT11(machine.Pin(CONFIG['sensor_pin']))
     d.measure()
     return d.temperature(),d.humidity()
 
 def load_config():
-    import ujson as json
     try:
         with open("/config.json") as f:
             config = json.loads(f.read())
@@ -40,7 +30,6 @@ def load_config():
         print("Loaded config from /config.json")
 
 def save_config():
-    import ujson as json
     try:
         with open("/config.json", "w") as f:
             f.write(json.dumps(CONFIG))
@@ -54,12 +43,9 @@ def main():
     while True:
         temphum = current_temp_humidity()
         client.publish('{}'.format(CONFIG['topic']),bytes((str(temphum[0]) + ","+ str(temphum[1])), 'utf-8'))
-        print('Current temperature: {}'.format(temphum[0]))
-        #client.publish('{}'.format(CONFIG['topic']),bytes(("Humidity: "+ str(temphum[1])), 'utf-8'))
-        print('Current humidity: {}'.format(temphum[1]))
-        time.sleep(10)
+        print('Published: T: {} H: {} '.format(temphum[0],temphum[1]))
+        time.sleep(60)
 
 if __name__ == '__main__':
     load_config()
-    setup_pins()
     main()
